3ff414532cf55eb2db1342b05201e861f11cc540
Sahil Sanjay Khane <97987621+SahilTheCodeBlitz@users.noreply.github.com>
2024-02-20 18:59:53 +0530

    Add files via upload
    Commiting Chessify

---
+++ index.js
@@ -0,0 +1,7 @@
+let bt = document.getElementById("register")
+
+window.location.href = "new_page.html";
+
+bt.addEventListener('click',()=>{
+    window.location.href = "http://127.0.0.1:5500/chess/index.html";
+})
